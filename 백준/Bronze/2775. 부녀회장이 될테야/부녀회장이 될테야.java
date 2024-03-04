963c518f71cd5ac9de986f00a7bdf1a83b783895
SACANDO <119462333+SACANDO@users.noreply.github.com>
2024-03-04 14:14:44 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 216 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int tc = sc.nextInt();
+		int[][] arr;
+		for (int t = 1; t <= tc; t++) {
+			int k = sc.nextInt();
+			int n = sc.nextInt();
+
+			arr = new int[k + 1][n + 1];
+
+			for (int i = 0; i < k + 1; i++) {
+				for (int j = 1; j < n + 1; j++) {
+					if (i == 0) {
+						arr[i][j] = j;
+					} else if (j == 1) {
+						arr[i][j] = 1;
+					} else {
+						arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+					}
+				}
+			}
+			System.out.println(arr[k][n]);
+		}
+	}
+}
